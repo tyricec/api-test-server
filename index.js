c15640ebcfcd81ff321977667369415e1346694d
Tyrice Clark <tyricec@civicresource.com>
2017-03-31 13:56:58 -0700

    change(*): Change reload to be 90 seconds

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ let timer = 0;
 setInterval(() => { timer += 1 }, 1000);
 
 app.get('/events', (req, res) => {
-  if (Math.floor(timer / 15) % 2 === 0) {
-    res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', '*')
+  if (Math.floor(timer / 90) % 2 === 0) {
     res.sendFile('events.json', { root: __dirname })
   } else {
     res.sendStatus(404)
