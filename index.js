c1685ceb7fbf76024d7fa78c5d2bfc3c78b2916e
Tyrice Clark <tyricec@civicresource.com>
2017-04-03 16:18:25 -0700

    feat(*): Add near transit api

--- index.js
+++ index.js
@@ -150,6 +150,15 @@ app.get('/gfs-operators', (req, res) => {
   }
 })
 
+app.get('/near-transit', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  if (Math.floor(timer / 50) % 2 === 0) {
+    res.sendFile('near-transit.json', { root: __dirname })
+  } else {
+    res.sendStatus(404)
+  }
+})
+
 let port = process.env.PORT || 9000
 
 app.listen(port)
