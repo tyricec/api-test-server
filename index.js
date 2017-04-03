cc478ec4f27ffeb6496875d99a3c31e0c4f64526
Tyrice Clark <tyricec@civicresource.com>
2017-04-03 16:10:46 -0700

    feat(*): Add citymaps api

--- index.js
+++ index.js
@@ -114,6 +114,15 @@ app.get('/bridges', (req, res) => {
   }
 })
 
+app.get('/citymaps', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  if (Math.floor(timer / 50) % 2 === 0) {
+    res.sendFile('citymaps.json', { root: __dirname })
+  } else {
+    res.sendStatus(404)
+  }
+})
+
 let port = process.env.PORT || 9000
 
 app.listen(port)
