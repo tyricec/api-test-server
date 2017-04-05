025e34496c34ecb3d1b89375a730f99874ce2a54
Tyrice Clark <tyricec@civicresource.com>
2017-04-05 14:38:07 -0700

    feat(*): Create api from json and display list of api

--- index.js
+++ index.js
@@ -8,202 +8,34 @@ let listeners = {}
 let timer = 0
 
 setInterval(() => { 
-  timer += 1 
+  timer += .05
   Object.keys(listeners).forEach((clientId) => {
     let socket = listeners[clientId]
 
-    let shortTimer = { short: (50 - timer % 50), isDown: Math.floor(timer / 50) % 2 === 1 }
-    let medTimer = { med: (70 - timer % 70), isDown: Math.floor(timer / 70) % 2 === 1}
-    let longTimer = { long: (90 - timer % 90), isDown: Math.floor(timer / 90) % 2 === 1 }
-
-    socket.write(createSocketMessage(JSON.stringify(shortTimer)))
-    socket.write(createSocketMessage(JSON.stringify(medTimer)))
-    socket.write(createSocketMessage(JSON.stringify(longTimer)))
+    socket.write(createSocketMessage(timer.toString()))
   }) 
-}, 1000)
+}, 50)
 
 app.get('/', (req, res) => {
   res.sendFile('client/index.html', { root: __dirname })
 })
 
-app.get('/events', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 90) % 2 === 0) {
-    res.sendFile('json/events.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
+var config = require('./api.json')
+
+config.forEach(api => {
+  app.get(api.endpoint, (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    if (Math.floor(timer / api.interval) % 2 === 0) {
+      res.sendFile(api.path, { root: __dirname })
+    } else {
+      res.sendStatus(404)
+    }
+  })
 })
 
-app.get('/tolls', (req, res) => {
+app.get('/config', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 70) % 2 === 0) {
-    res.sendFile('json/tolls.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/construction', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/construction.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/news', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/news.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/caltrans', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/caltrans.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/transit-alerts', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/transit-alerts.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/transit-centers', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/transit-centers.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/park-ride', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/park-ride.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/realtime-parking', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/realtime-parking.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/bike-trail-maps', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/bike-trail-maps.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/bridge-trail-maps', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/bridge-trail-maps.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/bridges', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/bridges.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/citymaps', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/citymaps.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/faq-categories', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/faq-categories.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/faqs', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/faqs.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/gfs-operators', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/gfs-operators.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/near-transit', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/near-transit.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/rtt-agencies', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/rtt-agencies.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/static-agencies', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/static-agencies.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-app.get('/vanpool', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  if (Math.floor(timer / 50) % 2 === 0) {
-    res.sendFile('json/vanpool.json', { root: __dirname })
-  } else {
-    res.sendStatus(404)
-  }
+  res.send(config)
 })
 
 app.use(express.static('client'))
