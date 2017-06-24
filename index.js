16009fd51c7d6dbf77106c5af2dfd67ef6e81c72
Tyrice Clark <tyricec@civicresource.com>
2017-06-23 23:47:59 -0700

    fix(*): Fix frontend panel to work with no api test server

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const crypto = require('crypto')
 const express = require('express')
+const EventEmitter = require('events')
 
 const app = express()
-const wsMagic = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
 
 let listeners = {}
 let apis = {}
-let timer = 0
 
-setInterval(() => { 
-  timer += .10
-  Object.keys(listeners).forEach((clientId) => {
-    let socket = listeners[clientId]
-
-    socket.write(createSocketMessage(timer.toString()))
-  }) 
-}, 100)
+const stateEmitter = new EventEmitter()
 
 app.enable('etag')
 
@@ -44,9 +39,6 @@ config.forEach(api => {
         res.setHeader(header, currentResponse.headers[header])
       })
     }
-    if (!currentResponse.headers || !currentResponse.headers['Cache-Control']) {
-      res.setHeader('Cache-Control', `public;max-age=${Math.floor(Math.abs(api.interval - timer))}`)
-    }
     if (currentResponse.response) {
       res.status(currentResponse.status)
       res.sendFile(currentResponse.response, { root: __dirname })
@@ -66,27 +58,35 @@ app.use(express.static('client'))
 
 let port = process.env.PORT || 9000
 
-app.listen(port).on('upgrade', (req, socket) => {
-  if (req.headers.upgrade === 'websocket') {
-    let hash = crypto.createHash('sha1')
-    let client = req.headers['sec-websocket-key']
-    hash.update(client + wsMagic, 'binary')
-    let handshake = hash.digest('base64')
-    let response = 'HTTP/1.1 101 Switching Protocols\r\n' +
-      'Upgrade: websocket\r\n' +
-      'Connection: Upgrade\r\n' +
-      'Sec-WebSocket-Accept: ' + handshake + '\r\n\r\n'
-    
-    listeners[client] = socket
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
 
-    socket.write(response)
+wss.on('connection', (ws, req) => {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send(JSON.stringify(apis))
+
+  stateEmitter.on('update', update)
+
+  ws.on('close', () => {
+   stateEmitter.removeListener('update', update)
+  })
+
+  function update() {
+    ws.send(JSON.stringify(apis))
   }
 })
+
+server.listen(port)
 
 function startResponseLifecycle(api) {
   const apiState = apis[api.name]
 
   const current = apiState.current
+
+  apiState.lastUpdated = Date.now()
 
   setTimeout(() => {
     const nextIndex = apiState.index + 1 >= apiState.responses.length ?
@@ -94,18 +94,11 @@ function startResponseLifecycle(api) {
 
     apiState.current = apiState.responses[nextIndex]
     apiState.index = nextIndex
+
+    stateEmitter.emit('update')
+
     startResponseLifecycle(api)
   }, current.timeToLive * 1000);
-}
-
-function createSocketMessage(message) {
-  let buf = Buffer.alloc(message.length + 2)
-
-  buf[0] = 0x81
-  buf.writeInt8(message.length, 1)
-  buf.write(message, 2) 
-
-  return buf;
 }
 
 console.log(`Started on port ${port}`)
